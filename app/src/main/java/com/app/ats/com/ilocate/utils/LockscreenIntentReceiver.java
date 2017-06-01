52df7d4351fcb0b3c25ba2c5b50f8b4368e261de
thanseehabdulla <thanseehabdulla@gmail.com>
2017-06-01 16:05:55 +0530

    initial

---
+++ app/src/main/java/com/app/ats/com/ilocate/utils/LockscreenIntentReceiver.java
@@ -0,0 +1,29 @@
+package com.app.ats.com.ilocate.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.app.ats.com.ilocate.MainActivity;
+
+public class LockscreenIntentReceiver extends BroadcastReceiver {
+
+	// Handle actions and display Lockscreen
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)
+				|| intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+			start_lockscreen(context);
+		}
+
+	}
+
+	// Display lock screen
+	private void start_lockscreen(Context context) {
+		Intent mIntent = new Intent(context, MainActivity.class);
+		mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(mIntent);
+	}
+
+}
